328a48df55ed3b26ce5a8ad5cb014d0a734d666e
Elvin Taghizade <elvintaghiyev184@gmail.com>
2020-02-13 18:07:44 +0400

    first homework solution commit No_#2 verion_2 with some advanced parts

--- src/main/java/hw01/hw_01.java
+++ src/main/java/hw01/hw_01.java
@@ -1,11 +1,13 @@
 package hw01;
 
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class hw_01 {
     public static void main(String[] args) {
-        //generating random number:
+
         Random random = new Random();
         int random_num = random.nextInt(101);
 
@@ -15,44 +17,44 @@ public class hw_01 {
 
         System.out.println("Let the game begin!");
 
-        int guessed_num, counter = 0;
-
-        int[] entered_nums = new int[100];
+        int guessed_num;
+        int[] entered_nums = new int[0];
 
         while (true) {
-            if (!(counter == 0)) {
-                System.out.print("Your numbers: [");
-                for (int i = 0; i < counter; i++) {
-                    System.out.print("\"" + entered_nums[i] + "\"");
-
-                    if (!(i == (counter - 1))) {
-                        System.out.print(", ");
-                    }
-                }
-                System.out.println("]");
-            } else {
-                System.out.println("You haven't guessed a number yet.");
-            }
-
             System.out.print("Please, enter a number [0:100]:  ");
             guessed_num = scanner.nextInt();
 
-            entered_nums[counter] = guessed_num;
+            entered_nums = fillingEnteredNums(entered_nums, guessed_num);
 
-            counter++;
-
-            if (guessed_num < random_num) {
+            if (guessed_num < random_num)
                 System.out.println("Your number is too small. Please, try again.");
-            } else if (guessed_num > random_num) {
+            else if (guessed_num > random_num)
                 System.out.println("Your number is too big. Please, try again.");
-            } else {
-                System.out.println("Congratulations, "
-                        + name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase()
-                        + "!");
-                System.out.println("You find hidden number in " + counter + " steps");
+            else {
+                System.out.printf("Congratulations, %s%s!\n",
+                        name.substring(0, 1).toUpperCase(),
+                        name.substring(1).toLowerCase());
+                System.out.printf("Your numbers: %s", Arrays.toString(entered_nums));
                 break;
             }
-        }//while
 
+
+        }
+
+    }
+
+    public static int[] fillingEnteredNums(int[] prevNums, int newNum) {
+        int newLen = prevNums.length + 1;
+        prevNums = Arrays.copyOf(prevNums, newLen);
+        prevNums[newLen - 1] = newNum;
+
+        Arrays.sort(prevNums);
+
+        int[] reversEnteredNums = new int[newLen];
+        for (int i = 0; i < newLen; i++) {
+            reversEnteredNums[newLen - 1 - i] = prevNums[i];
+        }
+
+        return reversEnteredNums;
     }
 }
